c4ba8d397611f7dbc743aecc3b76ea075d66ae1d
ahmettinastepe <ahmettinastepe0@gmail.com>
2020-03-20 23:51:14 +0300

    package.json and randomColor

--- helpers/randomColor.js
+++ helpers/randomColor.js
@@ -1,4 +1,4 @@
-const colors = ['blue', 'green', 'yellow', 'red', 'orange'];
+const colors = ['blue', 'green', 'red', 'pink', 'purple'];
 
 const getRandomColor = () => {
     return colors[Math.floor(Math.random() * colors.length)];
